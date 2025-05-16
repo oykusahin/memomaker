1329a3b585c943577899e68ec268946d668ab336
Oyku Sahin <oykusahin08@gmail.com>
2025-05-16 16:57:22 +0300

    add gradio app to main

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from db import models
 from db.database import engine
 from api.upload import router as upload_router 
 from api.items import router as items_router 
+from ui.gradio_ui import build_gradio_app
+from gradio.routes import mount_gradio_app
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -14,3 +16,6 @@ def root():
 
 app.include_router(upload_router, prefix="/api")
 app.include_router(items_router, prefix="/api")
+
+gradio_app = build_gradio_app()
+app = mount_gradio_app(app, gradio_app, path="/ui")
