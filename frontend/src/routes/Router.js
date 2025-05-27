6253c89948e63621af5ccb92a7b1d7c1c871083a
Oyku Sahin <oykusahin08@gmail.com>
2025-05-27 14:42:30 +0300

    Update the router for the new about-us menu pages

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -6,18 +6,23 @@ import LandingPage from "../pages/LandingPage";
 import StepUpload from "../pages/StepUpload";
 import StepGallery from "../pages/StepGallery";
 import AboutUs from "../pages/AboutUsPage";
-import ForWedding from "../pages/ForWeddingPage"
-import TestPage from "../pages/TestPage"
+import ForWedding from "../pages/ForWeddingPage";
+import TestPage from "../pages/TestPage";
+import AboutUsPage from "../pages/AboutUsPage";
+import Team from "../pages/TeamPage";
+import PrivacyPolicy from "../pages/PrivacyPolicyPage";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/about" element={<AboutUs/>} />
-      <Route path="/wedding" element={<ForWedding/>} />
+      <Route path="/about" element={<AboutUsPage />} />
+      <Route path="/wedding" element={<ForWedding />} />
       <Route path="/upload" element={<StepUpload />} />
       <Route path="/gallery" element={<StepGallery />} />
       <Route path="/test" element={<TestPage />} />
+      <Route path="/team" element={<Team />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
     </Routes>
   );
 };
