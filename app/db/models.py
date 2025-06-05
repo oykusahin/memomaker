d5eea4a7e41add0dabfa075bd6a544bebddc2a06
Oyku Sahin <oykusahin08@gmail.com>
2025-06-05 20:03:01 +0300

    Updated the DB PERSONS table to be able to store if the person is selected or not

--- app/db/models.py
+++ app/db/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Boolean, JSON
 from sqlalchemy.orm import relationship
 from db.database import Base
+from sqlalchemy.dialects.postgresql import ARRAY
+
 import uuid
 
 class Image(Base):
@@ -18,6 +20,7 @@ class Person(Base):
     id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
     name = Column(String, nullable=True)
     avatar_path = Column(String, nullable=True)
+    is_selected = Column(Boolean, default=False) 
     faces = relationship("Face", back_populates="person")
 
 class Face(Base):
@@ -29,8 +32,6 @@ class Face(Base):
     face_path = Column(String, nullable=True)
     person = relationship("Person", back_populates="faces")
     image = relationship("Image", back_populates="faces")
-
-from sqlalchemy.dialects.postgresql import ARRAY
 
 class ScrapbookItem(Base):
     __tablename__ = "scrapbook_items"
