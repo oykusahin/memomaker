c140016e8e1d867362fff465d21d0ea8da4bff41
Oyku Sahin <oykusahin08@gmail.com>
2025-06-16 11:42:28 +0300

    Add a list to be able add more origins in the future

--- app/main.py
+++ app/main.py
@@ -15,11 +15,14 @@ models.Base.metadata.create_all(bind=engine)
 
 # Initialize FastAPI app
 app = FastAPI()
+origins = [
+    "http://localhost:3000", 
+]
 
 # Enable CORS for frontend access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust this for your frontend
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
